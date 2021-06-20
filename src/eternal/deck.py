b7ebf5c6f9937ddde613b7893de84ed356d6f722
Jay Turcot <jay.turcot@affectiva.com>
2021-06-20 10:36:36 -0400

    Adding deck faction estimation.

--- src/eternal/deck.py
+++ src/eternal/deck.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
+
 class Deck:
     """Object representing a deck of cards in Eternal.
     """
+
     def __init__(self, main, market=[]):
         """Deck object
 
@@ -11,24 +13,58 @@ class Deck:
             market: List of Cards in the market
         """
         self.main_cards = main
-        self.main_data = pd.DataFrame( x.data for x in self.main_cards )
+        self.main_data = pd.DataFrame(x.data for x in self.main_cards)
 
         if market is not None:
             self.market_cards = market
-            self.market_data = pd.DataFrame( x.data for x in self.market_cards )
+            self.market_data = pd.DataFrame(x.data for x in self.market_cards)
         else:
             self.market_cards = None
             self.market_data = None
 
-    def faction(self, power_only_splash=False):
+    def _faction_counts(self):
+        """Get the count of non-power cards by faction.
+        NOTE: This does not count factionless cards
+
+        Returns: Dictionary with keys 'F','J','T','P','S' with values of the card-count
+        """
+
+        non_power_influence = self.main_data[self.main_data.Type != 'Power'].Influence
+        FACTIONS = set('FJTPS')
+        faction_counts = pd.Series(index=list(FACTIONS), data=0)
+        for faction in FACTIONS:
+            faction_counts[faction] = non_power_influence.str.contains(faction).sum()
+        return faction_counts
+
+    def faction(self):
         """Determine the faction of the deck based on main/market cards.
 
-        According to Reddit user /u/TheZardoz this is 10-cards on faction.
-        I assume that 1-9 cards consitutes a splash.
+        Logic for determining deck faction:
+         - 10 non-power cards shows a full pip (main color) in-game (when building a deck)
+         - 1-9 non-power cards show a full pip (splash color) in-game (when building a deck)
 
-        Source: https://www.reddit.com/r/EternalCardGame/comments/6zim9t/what_determines_what_faction_your_deck_is_in_an/
+        With 5 factions in a 75-card constructed deck (with 1/3 power requirement):
+        main factions are ones with >= 1/5th (5 factions total) of the non-power deck requirement
+
+        Adapted for Draft (45 card deck) we use 6 as the cutoff.
+
+        Returns: (main_factions, splash_factions) - Lists containing 'F','J','T','P' and/or 'S'
         """
-        raise NotImplementedError("Automatic determination of a Deck's faction(s) has yet to be implemented.")
+        MAIN_FACTION_COUNT = 6  # Adapted from 10 in constructed (to be confirmed)
+        faction_counts = self._faction_counts()
+        main_factions = faction_counts[faction_counts >= MAIN_FACTION_COUNT].index.tolist()
+        splash_factions = faction_counts[(faction_counts > 0) & (faction_counts < MAIN_FACTION_COUNT)].index.tolist()
+        return (main_factions, splash_factions)
+
+    def faction_string(self):
+        """Get deck faction strings
+
+        Returns: Faction string with splashes in lowercase
+        e.g. 'FT' for praxis and 'FJt' for rakano splashing time
+        """
+
+        main_factions, splash_factions = self.faction()
+        return ''.join(main_factions) + ''.join(splash_factions).lower()
 
     def types(self):
         """Return unit-type breakdown of the maindeck
@@ -41,4 +77,4 @@ class Deck:
 
         Returns: Series with mean unit stats
         """
-        return self.main_data[ self.main_data.Type == 'Unit' ][['Attack','Health']].mean()
+        return self.main_data[self.main_data.Type == 'Unit'][['Attack', 'Health']].mean()
